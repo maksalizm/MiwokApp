0228b4f36cdbccf8caebe0915f2f92a89261389c
pms <pms@wishket.com>
2016-10-12 20:34:45 +0900

    NumbersActivity에 숫자 구현

--- app/src/main/java/com/example/pms/miwokapp/NumbersActivity.java
+++ app/src/main/java/com/example/pms/miwokapp/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.example.pms.miwokapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -25,7 +27,14 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
-        Log.v("NumbersActivity", "Word at index 1: " + words.get(1));
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        int index = 0;
+        while( index < words.size()) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+            index++;
+        }
     }
 }
